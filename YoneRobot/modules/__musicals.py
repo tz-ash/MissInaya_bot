dd17d2f8df88f38437d2480d1f0f2b8f138d04c5
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-06-15 12:08:34 +0530

    Update __musicals.py

--- YoneRobot/modules/__musicals.py
+++ YoneRobot/modules/__musicals.py
@@ -7,5 +7,5 @@ __help__ = """
 /lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
 /glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
 𝑫𝑶 𝒀𝑶𝑼 𝑾𝑨𝑵𝑵𝑨 𝑴𝑼𝑺𝑰𝑪 𝑪𝑯𝑨𝑻
-𝚃𝙾 𝙷𝙴𝙰𝚁 𝙼𝚄𝚂𝙸𝙲 𝙾𝙽 𝚅𝙾𝙸𝙲𝙴 𝙲𝙷𝙰𝚃 𝙰𝙳𝙳  𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙶𝙸𝚅𝙴 𝙰𝙻𝙻 𝙰𝙳𝙼𝙸𝙽 𝙿𝙴𝚁𝙼𝙸𝙳𝚂𝙸𝙾𝙽𝚂 𝙰𝙽𝙳 𝙰𝙳𝙳 [@𝚝𝚣_𝚔𝚒𝚝𝚝𝚢 𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃](https://t.me/tz_kitty) 𝚃𝙾 𝙿𝙻𝙰𝚈 𝚃𝙷𝙴 𝙼𝚄𝚂𝙸𝙲.
+𝚃𝙾 𝙷𝙴𝙰𝚁 𝙼𝚄𝚂𝙸𝙲 𝙾𝙽 𝚅𝙾𝙸𝙲𝙴 𝙲𝙷𝙰𝚃 𝙰𝙳𝙳 @MissKitty_bot  𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙶𝙸𝚅𝙴 𝙰𝙻𝙻 𝙰𝙳𝙼𝙸𝙽 𝙿𝙴𝚁𝙼𝙸𝙳𝚂𝙸𝙾𝙽𝚂 𝙰𝙽𝙳 𝙰𝙳𝙳 [@𝚝𝚣_𝚔𝚒𝚝𝚝𝚢 𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃](https://t.me/tz_kitty) 𝚃𝙾 𝙿𝙻𝙰𝚈 𝚃𝙷𝙴 𝙼𝚄𝚂𝙸𝙲.
 """
